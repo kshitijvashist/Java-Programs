310e29e72f99f008ec90d6e359ac48e6ec870248
Kshitij Vashist <KSVA@tdc.dk>
2018-04-05 00:31:36 +0200

    add java class

---
+++ src/com/algo/runlengthencoding/RunlengthEncoding.java
@@ -0,0 +1,38 @@
+package com.algo.runlengthencoding;
+
+public class RunlengthEncoding {
+
+	// For example, if the input string is “wwwwaaadexxxxxx”,
+	// then the function should return “w4a3d1e1x6”.
+
+	public static void main(String args[]) {
+
+		String str = "aaaabbbccdddddddeeffffff";
+
+		String value = getRunLengthEncodingForGivenString(str);
+		System.out.println(value);
+	}
+
+	public static String getRunLengthEncodingForGivenString(String str) {
+		String value = "", compare = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			CharSequence seq = str.charAt(i) + "";
+
+			if (compare.contains(seq))
+				continue;
+
+			compare = compare + str.charAt(i);
+
+			int count = 0;
+			for (int j = 0; j < str.length(); j++) {
+				if (str.charAt(i) == str.charAt(j))
+					count = count + 1;
+			}
+
+			value = value + str.charAt(i) + Integer.toString(count);
+		}
+		return value;
+	}
+
+}
